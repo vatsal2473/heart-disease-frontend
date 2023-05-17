4fd619f4a6ddc735175a735056c5d8a843b44b4e
vatsal2473 <vatsal2473@gmail.com>
2023-05-17 15:46:12 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Heart Disease Prediction
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,150 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
+
 function App() {
+
+  const [formData, setFormData] = useState({
+
+    age: '',
+
+    sex: '',
+
+    cp: '',
+
+    trestbps: '',
+
+    chol: '',
+
+    fbs: '',
+
+    restecg: '',
+
+    thalach: '',
+
+    exang: '',
+
+    oldpeak: '',
+
+    slope: '',
+
+    ca: '',
+
+    thal: '',
+
+  });
+
+
+  const [prediction, setPrediction] = useState('');
+
+
+  const handleChange = (e) => {
+
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  };
+
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+
+    let formdata = new FormData();
+
+    for (const key in formData) {
+
+      formdata.append(key, formData[key]);
+
+    }
+
+
+    const requestOptions = {
+
+      method: 'POST',
+
+      body: formdata,
+
+      redirect: 'follow',
+
+    };
+
+
+    try {
+
+      const response = await fetch(
+
+        'https://heart-disease-api-production.up.railway.app/predict',
+
+        requestOptions,
+
+      );
+
+      const result = await response.text();
+
+      setPrediction(result);
+
+    } catch (error) {
+
+      console.log('error', error);
+
+    }
+
+  };
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Heart Disease Predictor</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        {Object.keys(formData).map((key) => (
+
+          <div key={key}>
+
+            <label htmlFor={key}>{key}:</label>
+
+            <input
+
+              type="text"
+
+              name={key}
+
+              id={key}
+
+              value={formData[key]}
+
+              onChange={handleChange}
+
+            />
+
+          </div>
+
+        ))}
+
+        <button type="submit">Predict</button>
+
+      </form>
+
+      {prediction && (
+
+        <div className="prediction">
+
+          Prediction: {prediction}
+
+        </div>
+
+      )}
+
     </div>
+
   );
+
 }
+
 
 export default App;
